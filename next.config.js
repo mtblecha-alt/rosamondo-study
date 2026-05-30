b9f775105452dcc088966217bd5410d3a73ca9e0
Marie-Therese <m.t.blecha@gmail.com>
2026-05-30 13:11:32 +0200

    Set dynamic base path for deployment and update environment variable in configuration

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
+const basePath = process.env.BASE_PATH ?? ''
+
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
-  basePath: '/rosamondo-study',
+  basePath,
   env: {
-    NEXT_PUBLIC_BASE_PATH: '/rosamondo-study',
+    NEXT_PUBLIC_BASE_PATH: basePath,
   },
   images: {
     unoptimized: true,
